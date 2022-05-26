578ba369cc86574792b10a540905a6130476332e
Kriyo <47378715+mytechs@users.noreply.github.com>
2022-05-26 12:16:54 +0530

    Updates

---
+++ precache-manifest.69f2e8c7745a52533d50ec542fd9fa5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "80e17aa995d91d24ae20",
+    "url": "/my-app/static/js/main.80e17aa9.chunk.js"
+  },
+  {
+    "revision": "52f9a7ff8525f5b837a3",
+    "url": "/my-app/static/js/1.52f9a7ff.chunk.js"
+  },
+  {
+    "revision": "80e17aa995d91d24ae20",
+    "url": "/my-app/static/css/main.e997c32c.chunk.css"
+  },
+  {
+    "revision": "52f9a7ff8525f5b837a3",
+    "url": "/my-app/static/css/1.78c30d36.chunk.css"
+  },
+  {
+    "revision": "28f44c2b8681892fef72b5e40f7aacbb",
+    "url": "/my-app/index.html"
+  }
+];
